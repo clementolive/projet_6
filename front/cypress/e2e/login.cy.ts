6af344f9f68a4bf8865db5b4c6702157d24f4157
Achmoye <achmoye@gmail.com>
2023-05-26 15:50:59 +0200

    Cypress config

---
+++ front/cypress/e2e/login.cy.ts
@@ -0,0 +1,18 @@
+
+describe('My First Test', () => {
+  it('Visits the initial project page', () => {
+    cy.visit('/');
+    cy.get('button').should("contain", "Se connecter");
+    cy.get('button').should("contain", "S'inscrire");
+
+    cy.visit('/login')
+    cy.get('label').should("contain", "E-mail");
+
+    cy.get('input[formControlName=email]').type("test@test.com")
+    cy.get('input[formControlName=password]').type(`${"Test!1234"}{enter}{enter}`)
+
+    cy.url().should('include', '/feed')
+
+
+  })
+})
